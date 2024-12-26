8cd14adf37f4efe7e7c4114ca062e66223ad724c
carsonng <ngcarson@gmail.com>
2024-12-26 08:47:00 +0800

    initial commit

---
+++ app/_layout.js
@@ -0,0 +1,5 @@
+import { Slot } from "expo-router";
+import { SessionProvider } from '../ctx';
+import "../global.css";
+
+export default Slot;
---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View style={{ paddingTop: 40 }} className="bg-red-200">
+      <Text className="text-3xl">
+        Home carson ng
+      </Text>
+    </View>
+  )
+}
